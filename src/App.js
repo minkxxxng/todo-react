01fa8d2a0822e477a5b3a191f4f3ecd5898e30c9
Eungyeol Jun <83086828+minkxxxng@users.noreply.github.com>
2023-05-18 19:47:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends React.Component { constructor(props){ //매개변수 props 생성자
  super(props); // 매개변수 pros 초기화
  this.state ={ // item 에 item.id, item.title, item.done 매개변수 이름과 값 할당
- items :[ {id:"todo0", title:"Todo 1 ", done:true}, {id:"todo1", title:"Todo 2 ", done:false}, ], }; } // add 함수 추가
+ items :[ {id:"todo0", title:"1913355 전은결 ", done:true}, {id:"todo1", title:"Todo 2 ", done:false}, ], }; } // add 함수 추가
  add =(item)=>{ const thisItems =this.state.items; item.id ="ID-"+ thisItems.length;//key를 위한 id 추가
  item.done =false; thisItems.push(item); this.setState({items:thisItems});//update state
  console.log("items:",this.state.items);
